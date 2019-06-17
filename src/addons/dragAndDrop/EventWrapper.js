9f108df5dace01901b2321db673405dd405b2342
Sarah Adams <sadams@dmsi.com>
2019-06-17 10:28:22 -0500

    Change to draggable event wrappers

--- src/addons/dragAndDrop/EventWrapper.js
+++ src/addons/dragAndDrop/EventWrapper.js
@@ -148,6 +148,7 @@ class EventWrapper extends React.Component {
     }
 
     if (
+      isDraggable &&
       draggable.dragAndDropAction.interacting && // if an event is being dragged right now
       draggable.dragAndDropAction.event === event // and it's the current event
     ) {
